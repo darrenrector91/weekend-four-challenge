6e66410e07980ec9a61b62b0be53ff38cf802ca5
Darren Rector <darrenrector@Darrens-MacBook-Pro.local>
2018-01-20 23:47:43 -0600

    images on DOM. NG-show/hide added

---
+++ server/public/scripts/client.js
@@ -0,0 +1,52 @@
+console.log('js sourced!');
+
+var app = angular.module('pictureApp', []);
+
+app.controller('PictureController', function ($http) {
+    console.log('PictureController loaded!');
+
+    var self = this;
+
+    // image data
+    self.images = [{
+            path: 'images/sara_darren_deke_wedding.jpg',
+            synopsis: "My wonderful Wife Sara and I"
+        },
+        {   
+            path: 'images/darren_iguana_shoulder.jpg',
+            synopsis: "Charlie the Iguana"
+        },
+        {
+            path: "images/stevie_couch.jpg",
+            synopsis: "The little one - Stevie"
+        },
+        {
+            path: "images/erik_walleye_sakakawea.jpg",
+            synopsis: "Walleye fishing Lake Sakakawea, ND"
+        },
+        {
+            path: "images/darren_brayden.jpg",
+            synopsis: "My nephew Brayden"
+        },
+        {
+            path: "images/cooper_closeup_bw.jpg",
+            synopsis: "The fluff ball - Cooper"
+        },
+        {
+            path: "images/darren_fishing_yawkey_tube.jpg",
+            synopsis: "Fly fishing"
+        },
+        {
+            path: "images/planes_on_ice.jpg",
+            synopsis: "Too funny!"
+        },
+        {
+            path: "images/carey_darren_twins_sox.jpg",
+            synopsis: "My Sister Carey and I @ Sox v. Twins"
+        }
+
+
+    ]
+
+
+});
